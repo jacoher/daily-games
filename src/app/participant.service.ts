bfe54297cca6393a6d2b10bf2962b8d867267a92
Jadir <jaco.dj@gmail.com>
2026-04-22 19:18:09 -0500

    Refactor: remove backend and use local storage

--- src/app/participant.service.ts
+++ src/app/participant.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Participant } from './participant.model';
 
 @Injectable({
@@ -8,46 +7,41 @@ import { Participant } from './participant.model';
 export class ParticipantService {
   participants: Participant[] = [];
   speedMultiplier = 1;
-  private apiUrl = 'http://localhost:3000/api/participants';
+  private storageKey = 'dailyGames_participants';
 
-  constructor(private http: HttpClient) {
+  constructor() {
     this.loadParticipants();
   }
 
   loadParticipants() {
-    this.http.get<{data: Participant[]}>(this.apiUrl).subscribe(response => {
-      this.participants = response.data;
-    });
+    const data = localStorage.getItem(this.storageKey);
+    if (data) {
+      try {
+        this.participants = JSON.parse(data);
+      } catch (e) {
+        console.error('Error parsing participants from localStorage', e);
+        this.participants = [];
+      }
+    } else {
+      this.participants = [];
+    }
+  }
+
+  private saveParticipants() {
+    localStorage.setItem(this.storageKey, JSON.stringify(this.participants));
   }
 
   addParticipant(name: string, avatarUrl: string) {
     if (name && !this.participants.find(p => p.name === name)) {
-      // Optimistic update
       this.participants = [...this.participants, { name, avatarUrl }];
-      
-      this.http.post(this.apiUrl, { name, avatarUrl }).subscribe({
-        next: () => this.loadParticipants(),
-        error: (err) => {
-          console.error('Error adding participant', err);
-          this.loadParticipants(); // revert on error
-        }
-      });
+      this.saveParticipants();
     }
   }
 
   removeParticipant(index: number) {
     if (index > -1 && index < this.participants.length) {
-      const pName = this.participants[index].name;
-      // Optimistic update
       this.participants = this.participants.filter((_, i) => i !== index);
-      
-      this.http.delete(`${this.apiUrl}/${encodeURIComponent(pName)}`).subscribe({
-        next: () => this.loadParticipants(),
-        error: (err) => {
-          console.error('Error removing participant', err);
-          this.loadParticipants(); // revert on error
-        }
-      });
+      this.saveParticipants();
     }
   }
 }
